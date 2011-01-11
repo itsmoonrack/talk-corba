591d8b3351aa523af3713605fb3ea9c03a8e6177
Sylvain <sylvain.lecoy@gmail.com>
2011-01-11 15:54:24 +0100

    Closing window

--- src/org/alma/corba/vue/SwingClient.java
+++ src/org/alma/corba/vue/SwingClient.java
@@ -116,44 +116,31 @@ public class SwingClient extends JFrame {
 
 		@Override
 		public void windowOpened(WindowEvent e) {
-			// TODO Auto-generated method stub
-			
 		}
 
 		@Override
 		public void windowClosing(WindowEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void windowClosed(WindowEvent e) {
-			// TODO Auto-generated method stub
 			mTalkDistant.stop(mConversationNumber);
 		}
 
 		@Override
+		public void windowClosed(WindowEvent e) {
+		}
+
+		@Override
 		public void windowIconified(WindowEvent e) {
-			// TODO Auto-generated method stub
-			
 		}
 
 		@Override
 		public void windowDeiconified(WindowEvent e) {
-			// TODO Auto-generated method stub
-			
 		}
 
 		@Override
 		public void windowActivated(WindowEvent e) {
-			// TODO Auto-generated method stub
-			
 		}
 
 		@Override
 		public void windowDeactivated(WindowEvent e) {
-			// TODO Auto-generated method stub
-			
 		}
 		
 	}
